9808a684a6085e8329af49f9c465eda256920bda
hameed <vjhameed3@gmail.com>
2020-03-15 22:08:28 +0500

    fix

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -5,6 +5,20 @@ export default class Redirect extends Component {
     if (!window.location.href.includes("https://www.security4startups.com")) {
       window.location.href = "https://www.security4startups.com"
     }
+
+    if (
+      window.location.href.includes("https://www.security4startups.com/SDLC")
+    ) {
+      window.location.href = "https://www.security4startups.com/sdlc"
+    }
+
+    if (
+      window.location.href.includes(
+        "https://www.security4startups.com/app-security"
+      )
+    ) {
+      window.location.href = "https://www.security4startups.com/app_security"
+    }
   }
 
   render() {
