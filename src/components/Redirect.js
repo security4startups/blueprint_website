59cab72ed7d3e3157f66920106eeaf9c1882bc01
hameed <vjhameed3@gmail.com>
2020-03-15 21:41:19 +0500

    fix

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -2,9 +2,9 @@ import React, { Component } from "react"
 
 export default class Redirect extends Component {
   componentDidMount() {
-    // if (!window.location.href.includes("https://www.security4startups.com")) {
-    //   window.location.href = "https://www.security4startups.com"
-    // }
+    if (!window.location.href.includes("https://www.security4startups.com")) {
+      window.location.href = "https://www.security4startups.com"
+    }
   }
 
   render() {
