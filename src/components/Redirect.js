fb71f0dc257122f3c340d7b78f96f3235371c0c9
hameed <vjhameed3@gmail.com>
2020-03-26 17:15:16 +0500

    fix

--- src/components/Redirect.js
+++ src/components/Redirect.js
@@ -2,12 +2,12 @@ import React, { Component } from "react"
 import { navigate } from "gatsby"
 export default class Redirect extends Component {
   componentDidMount() {
-    // if (!window.location.href.includes("https://www.security4startups.com")) {
-    //   const route =
-    //     "https://www.security4startups.com" + window.location.pathname
-    //   window.location.href = route
-    //   return
-    // }
+    if (!window.location.href.includes("https://www.security4startups.com")) {
+      const route =
+        "https://www.security4startups.com" + window.location.pathname
+      window.location.href = route
+      return
+    }
     // if (window.location.href.includes("/SDLC")) {
     //   navigate("/sdlc")
     //   return
